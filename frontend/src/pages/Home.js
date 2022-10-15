cbf416fdc3eb3423bcf603161f674e475b3ad097
golden2402 <golden2402@vt.edu>
2022-10-15 17:24:48 -0400

    add example content

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,10 @@ function MainPage() {
   return (
     <div className="home">
       <div className="page-card">
-        
+        <h1>Hello!</h1>
+        <p>
+          Welcome to Kidata, a kid-friendly introduction to data!
+        </p>
       </div>
     </div>
   )
