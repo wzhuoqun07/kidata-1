8ae336ea2830ee884e3713406020199ff98b911c
SahithKancharla <95754976+SahithKancharla@users.noreply.github.com>
2022-10-27 02:50:14 -0400

    Update Subject.java
    adding methods

--- backend/kidataMaven/src/main/java/tables/Subject.java
+++ backend/kidataMaven/src/main/java/tables/Subject.java
@@ -1,6 +1,10 @@
 package tables;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.json.simple.JSONObject;
 
 public class Subject {
     private static final String[] COLUMNS = {"id", "name"};
@@ -11,22 +15,31 @@ public class Subject {
         this.c = c;
     }
 
-    public void insert() {
+    public void insert(String[] vals) throws SQLException {
+        StringBuilder sql = new StringBuilder("INSERT IGNORE INTO Subject VALUES (");
+        sql.append(vals[2] + ", " + vals[3] + ");" );
+        System.out.println(sql);
+        c.createStatement().execute(sql.toString());
     }
 
-    public void delete() {
-
+    public void delete(String val) throws SQLException {
+        StringBuilder sql = new StringBuilder("DELETE FROM Subject WHERE ( id = " + val + ");"); 
+        System.out.println(sql);
+        c.createStatement().execute(sql.toString());
     }
 
-    public void update() {
-
-    }
-
-    public boolean check()  {
-        return false;
-    }
-
-    public String[] pull() {
-        return null;
+    @SuppressWarnings("unchecked")
+    public JSONObject pull(String val) throws SQLException {
+        StringBuilder sql = new StringBuilder("Select * From slides where UserID = " + val);
+        ResultSet rs = ((Statement) c.createStatement()).executeQuery(sql.toString());
+        JSONObject item = new JSONObject();
+        
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            item.put("id", id);
+            String name = rs.getString("name");
+            item.put("name", name);
+        }
+        return item;
     }
 }
