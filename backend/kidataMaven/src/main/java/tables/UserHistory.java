c9ca53eef3081b52557c833889f468a351cdc60c
SahithKancharla <95754976+SahithKancharla@users.noreply.github.com>
2022-10-27 02:51:27 -0400

    Update UserHistory.java

--- backend/kidataMaven/src/main/java/tables/UserHistory.java
+++ backend/kidataMaven/src/main/java/tables/UserHistory.java
@@ -1,6 +1,7 @@
 package tables;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UserHistory {
     private static final String[] COLUMNS = {"id", "user_id", "start_time", "end_time"};
@@ -11,22 +12,17 @@ public class UserHistory {
         this.c = c;
     }
 
-    public void insert() {
-    }
-
-    public void delete() {
-
-    }
-
-    public void update() {
-
-    }
-
-    public boolean check()  {
-        return false;
-    }
-
-    public String[] pull() {
-        return null;
+    public void insert(String[] vals) throws SQLException {
+        StringBuilder sql = new StringBuilder("INSERT IGNORE INTO userhistory VALUES ("); 
+        for (int i = 2; i < vals.length; i++) {
+            sql.append("'").append(vals[i]).append("'");
+            if (i != vals.length - 1) {
+                sql.append(", ");
+            }
+        }
+        sql.append(")");
+        sql.append(";");
+        System.out.println(sql);
+        c.createStatement().execute(sql.toString());
     }
 }
