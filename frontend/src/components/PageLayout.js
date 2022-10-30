433eb0b05c1f6e2bbc200ca37f0552410b93e212
LittleSugerNoice <116688730+LittleSugerNoice@users.noreply.github.com>
2022-10-30 17:05:33 -0400

    Add files via upload

---
+++ frontend/src/components/PageLayout.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import {Outlet} from "react-router-dom";
+
+
+function PageLayout() {
+  return (
+    <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+    </>
+  );
+}
+
+export default PageLayout;
