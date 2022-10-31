e3d853f5c59ebe962df178e2605adc8c69cc0f4d
LittleSugerNoice <yanglin1999624@vt.edu>
2022-10-31 18:20:01 -0400

    Update Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
-import PageLayout from "./pages/PageLayout"
+import PageLayout from "./components/PageLayout"
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import "./sass/main.scss"
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import Lesson from "./pages/Lesson";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import BasicGraph from "./pages/BasicGraph";
 
 function App() {
   return (
-    <PageLayout />
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<PageLayout />}>
+              <Route index element={<Home />}/>
+              <Route path={"/game"} element={<Game />}/>
+              <Route path={"/lesson"} element={<Lesson />}/>
+              <Route path={"/ContactUs"} element={<Contact />}/>
+              <Route path={"/Login"} element={<Login />}/>
+              <Route path={"/Lesson/BasicGraph"} element={<BasicGraph />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
   );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
   return (
     <div id="navbar-container">
-      <div className="navbar">
-        
+      <div className="navbar" >
+          <nav>
+              <NavLink to={"/"}>Home</NavLink> {"| "}
+              <NavLink to={"/lesson"}>Lesson</NavLink> {"| "}
+              <NavLink to={"/Game"}>Game</NavLink> {"| "}
+              <NavLink to={"/ContactUs"}>Contact Us</NavLink> {"| "}
+              <NavLink to={"/Login"}>Log in</NavLink>
+          </nav>
       </div>
     </div>
   );
