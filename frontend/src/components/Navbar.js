4f905fb1b323da270b8bca65c5e5e65078a14799
LittleSugerNoice <yanglin1999624@vt.edu>
2022-11-02 21:07:19 -0400

    Update pages

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,30 @@
 import {NavLink} from "react-router-dom";
 
+const activeStyle = {
+    color: 'red',
+    borderBottom: '1em, solid, black',
+    textDecoration: 'none'
+}
+
+const inactive = {
+    color: 'black',
+    borderBottom: '1em, solid, black',
+    textDecoration: 'none'
+}
+
 function Navbar() {
   return (
     <div id="navbar-container">
       <div className="navbar" >
           <nav>
-              <NavLink to={"/"}>Home</NavLink> {"| "}
-              <NavLink to={"/lesson"}>Lesson</NavLink> {"| "}
-              <NavLink to={"/Game"}>Game</NavLink> {"| "}
-              <NavLink to={"/ContactUs"}>Contact Us</NavLink> {"| "}
-              <NavLink to={"/Login"}>Log in</NavLink>
+              <NavLink style={({isActive}) => isActive ? activeStyle : inactive}
+                       to={"/"} end>Home</NavLink> {"| "}
+              <NavLink style={({isActive}) => isActive ? activeStyle : inactive}
+                       to={"/Lesson"}>Lesson</NavLink> {"| "}
+              <NavLink style={({isActive}) => isActive ? activeStyle : inactive}
+                       to={"/ContactUs"}>Contact Us</NavLink> {"| "}
+              <NavLink style={({isActive}) => isActive ? activeStyle : inactive}
+                       to={"/Login"}>Log in</NavLink>
           </nav>
       </div>
     </div>
