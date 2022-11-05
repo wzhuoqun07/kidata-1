617fd17269ec1af6a49807a9379702e72e00c51a
golden2402 <golden2402@vt.edu>
2022-11-05 01:57:41 -0400

    change header and navbar profile

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,12 @@ import Navbar from "./Navbar";
 
 function Header() {
   return (
-    <div id="header">
-      <div className="header-card">
-        <div className="header-content">
-          <img 
-            src="/"
-            className="logo"
-          />
-          <h1>KIDATA (temporary title)</h1>
-        </div>
-        <Navbar />
-      </div>
+    <div className="header">
+      <img
+        id="logo"
+        src="/"
+      />
+      <Navbar />
     </div>
   );
 }
