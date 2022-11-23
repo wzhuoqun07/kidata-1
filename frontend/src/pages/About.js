2da14cc9f7abc564e430e1ed73e014e5836db55c
golden2402 <golden2402@vt.edu>
2022-11-23 09:16:43 -0500

    change .title class's scope

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -4,9 +4,9 @@ function About() {
   return (
     // this is a ridiculously lazy solution, want to replace with markdown:
     <>
-      <h1>About KIDATA</h1>
+      <h1 className="title">About KIDATA</h1>
       <p>
-        
+        This is where we'd put information about the KIDATA project.
       </p>
     </>
   )
