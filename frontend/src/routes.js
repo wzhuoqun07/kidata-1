c0fa33013f228ebcfd83e74ada90a309de4a0050
golden2402 <golden2402@vt.edu>
2022-11-23 09:17:13 -0500

    add Lessons skeleton

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -9,13 +9,16 @@ import PageLayout from "./components/PageLayout";
 import Home from "./pages/Home"
 import About from "./pages/About";
 import Error from "./pages/Error";
+import Lessons from "./pages/Lessons";
 
 const router = createBrowserRouter(
   createRoutesFromElements([
     // root pages:
     <Route path="/" element={<PageLayout />}>
       <Route index element={<Home />} />
+      
       <Route path="/about" element={<About />} />
+      <Route path="/lessons" element={<Lessons />} />
 
       {/* error page: */}
       <Route path="*" element={<Error />} />
