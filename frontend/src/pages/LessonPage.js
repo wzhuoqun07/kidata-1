630fe06622b823bbe8a5baa579a8ffee344f2520
golden2402 <golden2402@vt.edu>
2022-11-25 21:07:06 -0500

    skeleton LessonPage components

--- frontend/src/pages/LessonPage.js
+++ frontend/src/pages/LessonPage.js
@@ -3,6 +3,50 @@ import { useEffect, useState } from "react";
 
 import Markdown from "markdown-to-jsx";
 
+function QuizChoice(props) {
+  return (
+    <li>
+      <button
+        // this needs some revision; may want to elevate this to be at the same
+        // level as the component  
+        onClick={event => props.onClick(event)}
+      >
+        { props.text }
+      </button>
+    </li>
+  )
+}
+
+function QuizQuestions(props) {
+  const { question, choices, answer } = props.quiz;
+
+  return (
+    <div>
+      <h1>{ question }</h1>
+      {
+        // questions/choices should be returned as an array when stringify-ed,
+        // so use map:
+        choices.map((choice, i) => (
+          // ...and return a div for each array entry:
+          <QuizChoice 
+            text={`${String.fromCharCode(parseInt('A') + i)}. ${choice}`}
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+function LessonContent(props) {
+  return (
+    <>
+      <Markdown children={ props.markdown } />
+      {/* if we don't have a quiz, just don't render anything here: */}
+      { props.quiz && <QuizQuestions quiz={props.quiz} /> }
+    </>
+  )
+}
+
 function FetchError() {
   return (
     <div>
@@ -16,15 +60,17 @@ function LessonPage() {
 
   const [ content, setContent ] = useState(null);
   const [ failure, setFailure ] = useState(false);
-
+  
   useEffect(() => {
     fetch("http://localhost:8080/pull/slides/" + id)
       .then(response => response.json())
       // apply lesson content:
       .then(body => {
+        // if we have no markdown, need to invoke an error:
         if (!body.markdown)
-          console.log("Uh oh.");
-
+          // invoke the .catch:
+          throw "Didn't receive markdown body from server.";
+        
         setContent(body.markdown);
       })
       .catch(reason => {
@@ -35,7 +81,7 @@ function LessonPage() {
 
   return (
     <div className="markdown-content">
-      {content ? <Markdown children={content} /> : <FetchError/>}
+      
     </div>
   )
 }
